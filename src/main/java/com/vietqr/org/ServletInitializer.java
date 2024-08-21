7acbe0557cf063d47bf632149fa640e0bf7b71f8
hoangan-vietqr <an.nh@vietqr.vn>
2024-08-21 16:06:18 +0700

    Implement ServletInitializer

---
+++ src/main/java/com/vietqr/org/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.vietqr.org;
+
+import com.vietqr.org.VietqrBizServerApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VietqrBizServerApplication.class);
+    }
+
+}
