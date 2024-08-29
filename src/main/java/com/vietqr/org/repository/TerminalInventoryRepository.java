29c0445030f5b15d1642f4d069e8d69fa579cd78
long.lp <long.lp@vietqr.vn>
2024-08-29 14:39:50 +0700

    refactor create entity

--- src/main/java/com/vietqr/org/repository/TerminalInventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vietqr.org.repository;
-
-import com.vietqr.org.entity.TerminalInventoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TerminalInventoryRepository extends JpaRepository<TerminalInventoryEntity, String> {
-}
